5cc2466c8f105ab0da60d6ac4d0df810c35c308b
leonardo <leonardo.victorufpa@gmail.com>
2019-07-25 14:19:25 -0300

    JWT authentication

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 /**
  * Route to create user;
  */
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
