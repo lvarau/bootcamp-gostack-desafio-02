eb7724a6e49fefa297d292f49b3bc4c650f74f66
leonardo <leonardo.victorufpa@gmail.com>
2019-07-25 10:55:08 -0300

    Initiating loader of models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
-
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+/**
+ * Testing route;
+ * Creating an user;
+ * Saving in database;
+ */
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Leonardo',
+    email: 'leonardo@leonardo.com',
+    password_hash: '123456789'
+  });
+  return res.json(user);
+});
 
 export default routes;
